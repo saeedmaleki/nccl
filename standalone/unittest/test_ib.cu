0dd2df8338a500727f3601693042c8c983c567f1
wusar <wusar@mail.ustc.edu.cn>
2023-01-31 07:03:44 +0000

    add the ib send recv

--- standalone/unittest/test_ib.cu
+++ standalone/unittest/test_ib.cu
@@ -1,29 +1,56 @@
 #include "net_ib.h"
 #include <mpi.h>
+#define bytes 1024
+int ib_send()
+{
+    char *sendbuff = (char *)malloc(bytes);
+    for (int i = 0; i < bytes; i++) {
+        sendbuff[i] = i;
+    }
+    ncclIbHandle handle;
+    ncclIbSendComm *sendComm;
+    NCCLCHECK(ncclIbConnect(1, &handle, (void **)&sendComm));
+    ibv_mr *mhandle;
+    NCCLCHECK(ncclIbRegMr(sendComm, sendbuff, bytes, NCCL_PTR_HOST,
+                          (void **)&mhandle));
+    // NCCLCHECK(ncclIbIsend(sendComm, sendbuff, bytes, 0, 0, 0));
+}
+
+int ib_recv()
+{
+    char *recvbuff = (char *)malloc(bytes);
+    ncclIbHandle handle;
+    ncclIbListenComm *listenComm;
+
+    NCCLCHECK(ncclIbListen(0, &handle, (void **)&listenComm));
+    ncclIbRecvComm *recvComm;
+    NCCLCHECK(ncclIbAccept(listenComm, (void **)&recvComm));
+    ibv_mr *mhandle;
+    NCCLCHECK(ncclIbRegMr(recvComm, recvbuff, bytes, NCCL_PTR_HOST,
+                          (void **)&mhandle));
+    // NCCLCHECK(ncclIbIrecv(recvComm, recvbuff, bytes, 0, 0, 0));
+}
 
 int ib_sendrecv_test(int world_rank)
 {
+    printf("ib_sendrecv_test rankd:%d\n", world_rank);
     ncclDebugLogger_t logger;
-    ncclIbInit(logger);
+    NCCLCHECK(ncclIbInit(logger));
     int devicenum;
     ncclNetIb.devices(&devicenum);
     printf("devicenum=%d", devicenum);
     ncclNetProperties_t properties;
-    ncclIbGetProperties(0, &properties);
-    printf("name=%s, pciPath=%s, guid=%lu, ptrSupport=%d, speed=%d, port=%d, "
-           "latency=%f, maxComms=%d, maxRecvs=%d\n",
-           properties.name, properties.pciPath, properties.guid,
-           properties.ptrSupport, properties.speed, properties.port,
-           properties.latency, properties.maxComms, properties.maxRecvs);
-    ncclIbHandle handle;
-    ncclIbListenComm *listenComm;
-    ncclIbListen(0, &handle, (void **)&listenComm);
+    NCCLCHECK(ncclIbGetProperties(0, &properties));
+    if (world_rank == 0) {
+        ib_send();
+    } else if (world_rank == 1) {
+        ib_recv();
+    }
 }
 
 int main()
 {
     setbuf(stdout, NULL);
-    
 
     MPI_Init(NULL, NULL);
     // Get the number of processes
