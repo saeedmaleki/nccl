9e2ff8dddbab54a5e1b7c5a0aaafd695e4d37875
wusar <wusar@mail.ustc.edu.cn>
2023-01-31 08:53:25 +0000

    add the socket address and port

--- standalone/unittest/test_ib.cu
+++ standalone/unittest/test_ib.cu
@@ -1,46 +1,62 @@
 #include "net_ib.h"
 #include <mpi.h>
 #define bytes 1024
+#define TAG 7
+#define PORT 8080
 int ib_send()
 {
+    ncclDebugLogger_t logger;
+
+    NCCLCHECK(ncclIbInit(logger));
     char *sendbuff = (char *)malloc(bytes);
     for (int i = 0; i < bytes; i++) {
         sendbuff[i] = i;
     }
     ncclIbHandle handle;
     ncclIbSendComm *sendComm;
-    NCCLCHECK(ncclIbConnect(1, &handle, (void **)&sendComm));
+    ncclIbListenComm *listenComm;
+    handle.connectAddr.sin.sin_family = AF_INET;
+    handle.connectAddr.sin.sin_port = htons(PORT);
+    inet_aton("127.0.0.1", &handle.connectAddr.sin.sin_addr);
+
+    NCCLCHECK(ncclIbConnect(0, &handle, (void **)&sendComm));
     ibv_mr *mhandle;
-    NCCLCHECK(ncclIbRegMr(sendComm, sendbuff, bytes, NCCL_PTR_HOST,
-                          (void **)&mhandle));
-    // NCCLCHECK(ncclIbIsend(sendComm, sendbuff, bytes, 0, 0, 0));
+    // NCCLCHECK(ncclIbRegMr(sendComm, sendbuff, bytes, NCCL_PTR_HOST,
+    //                       (void **)&mhandle));
+    // NCCLCHECK(ncclIbIsend(sendComm, sendbuff, bytes, TAG, mhandle, 0));
 }
 
 int ib_recv()
 {
+    ncclDebugLogger_t logger;
+
+    NCCLCHECK(ncclIbInit(logger));
     char *recvbuff = (char *)malloc(bytes);
     ncclIbHandle handle;
     ncclIbListenComm *listenComm;
+    handle.connectAddr.sin.sin_family = AF_INET;
+    handle.connectAddr.sin.sin_port = htons(PORT);
+    inet_aton("127.0.0.1", &handle.connectAddr.sin.sin_addr);
 
     NCCLCHECK(ncclIbListen(0, &handle, (void **)&listenComm));
-    ncclIbRecvComm *recvComm;
-    NCCLCHECK(ncclIbAccept(listenComm, (void **)&recvComm));
-    ibv_mr *mhandle;
-    NCCLCHECK(ncclIbRegMr(recvComm, recvbuff, bytes, NCCL_PTR_HOST,
-                          (void **)&mhandle));
-    // NCCLCHECK(ncclIbIrecv(recvComm, recvbuff, bytes, 0, 0, 0));
+    // ncclIbRecvComm *recvComm;
+    // NCCLCHECK(ncclIbAccept(listenComm, (void **)&recvComm));
+    // ibv_mr *mhandle;
+    // NCCLCHECK(ncclIbRegMr(recvComm, recvbuff, bytes, NCCL_PTR_HOST,
+    //                       (void **)&mhandle));
+    // int size = bytes;
+    // int tag = TAG;
+    // NCCLCHECK(ncclIbIrecv(recvComm, 1, (void **)&recvbuff, &size, &tag,
+    //                       (void **)&mhandle, 0));
 }
 
 int ib_sendrecv_test(int world_rank)
 {
-    printf("ib_sendrecv_test rankd:%d\n", world_rank);
-    ncclDebugLogger_t logger;
-    NCCLCHECK(ncclIbInit(logger));
-    int devicenum;
-    ncclNetIb.devices(&devicenum);
-    printf("devicenum=%d", devicenum);
-    ncclNetProperties_t properties;
-    NCCLCHECK(ncclIbGetProperties(0, &properties));
+    // int devicenum;
+    // ncclNetIb.devices(&devicenum);
+    // printf("devicenum=%d", devicenum);
+    // ncclNetProperties_t properties;
+    // NCCLCHECK(ncclIbGetProperties(0, &properties));
     if (world_rank == 0) {
         ib_send();
     } else if (world_rank == 1) {
